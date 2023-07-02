e642c42d3ddb020204fe418b60cf665cc9b99a7d
Andrew Chulkov <andrey.chulkov@list.ru>
2023-07-02 13:48:39 +0300

    js burger

--- js/main.js
+++ js/main.js
@@ -1,35 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
-  modals()
+    modals()
 });
 
 const modals = () => {
-  function popupModal(modalSelector, closeSelector, targetSelector) {
-    const modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector),
-      target = document.querySelector(targetSelector);
+    function popupModal(modalSelector, closeSelector, targetSelector) {
+        const modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector),
+            target = document.querySelector(targetSelector);
 
-    close.addEventListener('click', () => {
-      modal.style.display = "none";
-      document.querySelector('body').style.overflow = "";
-    })
+        close.addEventListener('click', () => {
+            modal.style.display = "none";
+            document.querySelector('body').style.overflow = "";
+        })
 
-    modal.addEventListener('click', (e) => {
-      if (e.target === target) {
-        modal.style.display = "none";
-        document.querySelector('body').style.overflow = "";
-      }
-    })
-  }
+        modal.addEventListener('click', (e) => {
+            if (e.target === target) {
+                modal.style.display = "none";
+                document.querySelector('body').style.overflow = "";
+            }
+        })
+    }
 
-  function showModalByTime(selector, time) {
-    setTimeout(function () {
-      document.querySelector(selector).style.display = "block";
-      document.querySelector('body').style.overflow = "hidden";
-    }, time);
-  }
+    function showModalByTime(selector, time) {
+        setTimeout(function () {
+            document.querySelector(selector).style.display = "block";
+            document.querySelector('body').style.overflow = "hidden";
+        }, time);
+    }
 
-  popupModal('.popup', '.popup-close', '.popup-body')
+    function showNavBurger() {
+        const burger = document.querySelector(".header-burger");
+        const nav = document.querySelector(".header-nav");
 
-  showModalByTime(".popup", 3000);
+        burger.addEventListener('click', () => {
+            if (window.getComputedStyle(nav).display === "block") {
+                nav.style.display = "none";
+            } else {
+                nav.style.display = "block";
+            }
+        })
+    }
+
+    popupModal('.popup', '.popup-close', '.popup-body');
+
+    showModalByTime(".popup", 3000);
+
+    showNavBurger('.header-burger', ".header-nav");
 }
+
+
+
+
+
+
+
 
