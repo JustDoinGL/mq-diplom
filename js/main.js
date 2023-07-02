9456d48da14177ab794e971a326b708011920698
Andrew Chulkov <andrey.chulkov@list.ru>
2023-07-02 10:33:43 +0300

    create popup and js

---
+++ js/main.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+  modals()
+});
+
+const modals = () => {
+  function popupModal(modalSelector, closeSelector, targetSelector) {
+    const modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector),
+      target = document.querySelector(targetSelector);
+
+    close.addEventListener('click', () => {
+     modal.style.display = "none";
+    })
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === target) {
+        modal.style.display = "none";
+      }
+
+    })
+
+  }
+
+  function showModalByTime(selector, time) {
+    setTimeout(function () {
+      document.querySelector(selector).style.display = "block";
+    }, time);
+  }
+
+  popupModal('.popup', '.popup-close', '.popup-body')
+
+  showModalByTime(".popup", 3000);
+}
